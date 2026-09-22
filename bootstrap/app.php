394d8999b008db123c731de0b55db9a752a3c456
lucimavil <99848710+lucimavil@users.noreply.github.com>
2026-09-22 15:55:48 -0300

    Devolver JSON cuando el adjunto supera el limite del servidor

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Exceptions\PostTooLargeException;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +16,18 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (PostTooLargeException $e, Request $request) {
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'message' => 'El archivo adjunto supera el tamaño máximo permitido por el servidor.',
+                    'errors' => [
+                        'adjunto' => [
+                            'El archivo adjunto es demasiado grande. El máximo permitido es 10 MB.'
+                        ],
+                    ],
+                ], 413);
+            }
+
+            return null;
+        });
     })->create();
